deb151c0e3db2563d1f0b78e85fb59efe1835fd6
Rohnak Agarwal <rrka79wal@gmail.com>
2021-06-18 01:15:47 +0530

    single file complete

--- website/main/routes.py
+++ website/main/routes.py
@@ -20,16 +20,21 @@ def editor(file_id):
         os.mkdir(files_dir)
     if not os.path.exists(file):
         open(file, 'w').close()
-    return render_template('editor.html', file_id=file_id, save=save)
+    with open(file) as f:
+        text = f.read()
+    return render_template('editor.html', file_id=file_id, save=save, text=text)
 
 
-def generate_file_id():
+@main_bp.route('/view/<file_id>')
+def view(file_id):
+    file = os.path.join(files_dir, file_id + '.txt')
     if not os.path.exists(files_dir):
-        os.mkdir(files_dir)
-    file_id = uuid.uuid4().hex
-    while os.path.exists(os.path.join(files_dir, file_id + '.txt')):
-        file_id = uuid.uuid4().hex
-    return file_id
+        abort(400)
+    file = open(file)
+    text = file.read()
+    file.close()
+    # text = text.replace('\n', '<br>')
+    return render_template('viewer.html', text=text, file_id=file_id)
 
 
 @main_bp.route('/save/<file_id>', methods=['POST'])
@@ -41,8 +46,23 @@ def save(file_id):
             if os.path.exists(file):
                 with open(file, 'w') as file_writeable:
                     file_writeable.write(text)
-                with open(file) as file_readable:
-                    print(file_readable.read())
+                # with open(file) as file_readable:
+                #     print(file_readable.read())
                 return '200'
             return '404'
     return '500'
+
+
+def generate_file_id():
+    if not os.path.exists(files_dir):
+        os.mkdir(files_dir)
+    file_id = uuid.uuid4().hex
+    while os.path.exists(os.path.join(files_dir, file_id + '.txt')):
+        file_id = uuid.uuid4().hex
+    return file_id
+
+#================errors========================
+
+@main_bp.app_errorhandler(400)
+def error_400():
+    pass
